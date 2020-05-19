5ba35eb40a62765017387f06236693015d5a3b33
Stefan Richter <stefan@modul1.de>
2020-05-20 00:33:51 +0200

    update with my stuff

--- course-04/exercises/c4-demos-master/08-dynamodb-create/index.js
+++ course-04/exercises/c4-demos-master/08-dynamodb-create/index.js
@@ -7,28 +7,28 @@ const docClient = new AWS.DynamoDB.DocumentClient()
 const groupsTable = process.env.GROUPS_TABLE
 
 exports.handler = async (event) => {
-  console.log('Processing event: ', event)
-  const itemId = uuid.v4()
+    console.log('Processing event: ', event)
+    const itemId = uuid.v4()
 
-  const parsedBody = JSON.parse(event.body)
+    const parsedBody = JSON.parse(event.body)
 
-  const newItem = {
-    id: itemId,
-    ...parsedBody
-  }
+    const newItem = {
+        id: itemId,
+        ...parsedBody
+    }
 
-  await docClient.put({
-    TableName: groupsTable,
-    Item: newItem
-  }).promise()
+    await docClient.put({
+        TableName: groupsTable,
+        Item: newItem
+    }).promise()
 
-  return {
-    statusCode: 201,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      newItem
-    })
-  }
+    return {
+        statusCode: 201,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            newItem
+        })
+    }
 }
--- course-04/exercises/lesson-2/client/src/config.ts
+++ course-04/exercises/lesson-2/client/src/config.ts
@@ -1,1 +1,1 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://ng17fbkqxf.execute-api.eu-west-1.amazonaws.com/dev'
